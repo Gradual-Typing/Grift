3c5a8e1eb410252dc84a11c84e8d9ab2066d2e5d
akuhlens <akuhlens@localhost.localdomain>
2014-05-16 11:42:35 -0400

    changed my opinion of what a pretty printer should be

--- language/core.rkt
+++ language/core.rkt
@@ -1,19 +1,150 @@
 #lang racket
 (require Schml/language/types
          Schml/language/shared)
+
+(require racket/generic
+	 racket/format)
+
+(define (display/indent o i p)
+  (display o p))
+
+(define not-Expr (lambda (o) (not (Expr? o))))
+
+(define-generics pretty-print
+  (pprint pretty-print indent port)
+  #:defaults ([not-Expr 
+	       (define pprint display/indent)])
+  #:fallbacks (display/indent))
+
 (provide (all-defined-out))
 
-(struct Prog (name type expr*))
-(struct Expr (src type))
-(struct Lambda Expr (fmls body))
-(struct Var Expr (ident))
-(struct App Expr (expr expr*))
-(struct Cast Expr (expr blame))
-(struct If Expr (test conseq alt))
-(struct Let Expr (dec* body))
+(define (indent i p)
+  (do ((n 0 (add1 n))) ((= n i)) (display #\space)))
+
+(define (write-prog p port mode)
+  (write-string "\n(Prog: " port)
+  (write-string (Prog-name p) port)
+  (write-string " Type: " port)
+  (write-string (~a (Prog-type p)) port)
+  (for ((e (in-list (Prog-expr* p))))
+    (newline port)
+    (indent 4 port)
+    (pprint e 4 port))
+  (write-string ")" port))
+
+(struct Prog (name type expr*)
+	#:methods gen:custom-write
+	[(define write-proc write-prog)])
+    
+(struct Expr (src type)
+	 #:methods gen:pretty-print
+	 [(define (pprint o i p)
+	    (indent i p)
+	    (write-string "<Expr>" p))])
+
+(define (pprint-lambda o i p)
+  (write-string "(lambda (" p)
+  (let ((fml* (Lambda-fmls o)))
+    (if (null? fml*)
+	(write-string "): " p)
+	(begin 
+	  (pprint (car fml*) (+ 5 i) p)
+	  (let loop ((fml*  (cdr fml*)))
+	    (if (null? fml*)
+		(write-string "): " p)
+		(begin
+		  (write-string " " p)
+		  (pprint (car fml*) (+ 5 i) p)
+		  (loop (cdr fml*))))))))
+  (pprint (Expr-type o) (+ 5 i) p)
+  (newline p)
+  (indent (+ 3 i) p)
+  (pprint (Lambda-body o) (+ 3 i) p)
+  (write-string ")" p))
+
+(struct Lambda Expr (fmls body)
+	#:methods gen:pretty-print
+	[(define pprint pprint-lambda)])
+
+(struct Var Expr (ident)
+	#:methods gen:pretty-print
+	[(define pprint 
+	   (lambda (o i p) (display (Var-ident o) p)))])
+
+(define (pprint-app o i p)
+  (display #\( p)
+  (pprint (App-expr o) (add1 i) p)
+  (for ((e (in-list (App-expr* o))))
+    (display #\space p)
+    (pprint e (+ 4 i) p))
+  (display #\) p))
+
+(struct App Expr (expr expr*)
+	#:methods gen:pretty-print
+	[(define pprint pprint-app)])
+
+(define (pprint-cast o i p)
+  (display "(: " p)
+  (pprint (Cast-expr o) (add1 i) p)
+  (fprintf p " ~a ~a)" (Expr-type o) (Cast-blame o)))
+
+(struct Cast Expr (expr blame)
+	#:methods gen:pretty-print
+	[(define pprint pprint-cast)])
+
+(define (pprint-if o i p)
+  (display "(if " p)
+  (pprint (If-test o) (+ 4 i) p)
+  (newline p)
+  (pprint (If-conseq o) (+ 4 i) p)
+  (newline p)
+  (pprint (If-alt o) (+ 4 i) p)
+  (display ")" p))
+  
+(struct If Expr (test conseq alt)
+	#:methods gen:pretty-print
+	[(define pprint pprint-if)])
+
+(define (pprint-let o i p)
+  (write-string "(let (" p)
+  (for ((bnd (in-list (Let-bnds o))))
+    (pprint bnd (+ 6 i) p)
+    (newline p))
+  (write-string ")\n" p)
+  (indent (+ 3 i) p)
+  (pprint (Let-body o) (+ 3 i) p)
+  (display ")" p))
+
+(struct Let Expr (bnds body)
+	#:methods gen:pretty-print
+	[(define pprint pprint-let)])
 (struct Const Expr (const))
-(struct Bnd (ident type expr))
-(struct Fml (ident type))
+
+(define (pprint-bnd o i p)
+  (write-string "(" p)
+  (display (Bnd-ident o) p)
+  (when (Bnd-type o) 
+    (write-string " :" p)
+    (display (Bnd-type o) p))
+  (write-string " " p)
+  (pprint (Bnd-expr o) (+ 4 i) p)
+  (write-string "(" p))
+
+(struct Bnd (ident type expr)
+	#:methods gen:pretty-print
+	[(define pprint pprint-bnd)])
+
+(define (pprint-fml o i p)
+  (when (Fml-type o) (write-string "(" p))
+  (display (Fml-ident o) p)
+  (when (Fml-type o)
+    (write-string " : " p)
+    (display (Fml-type o) p)
+    (write-string ")" p)))
+
+(struct Fml (ident type)
+	#:methods gen:pretty-print
+	[(define pprint pprint-fml)])
 
 (struct Prim Expr ())
 (struct Prim:Rel Prim ())
@@ -32,6 +163,17 @@
 (struct Prim:Bin:Int:or  Prim:Bin:Int ())
 (struct Prim:Bin:Int:>>  Prim:Bin:Int ())
 (struct Prim:Bin:Int:<<  Prim:Bin:Int ())
+
+
+;; (define (ast-pp ast port mode)
+  
+;;   (let-values (((l c p) (port-next-location port)))
+;;     (let ((c (or c (begin (newline p) 0))))
+;;       (fprintf port "(Program ~s\n" (Prog-name))
+;;       (for ((e (in-list (Prog-expr* ast))))
+;; 	(for ((i (in-range
+;; 	(fprintf (pp (Prog)
+
 
 
 (define (constant? x)
@@ -86,3 +228,13 @@
   (and (Prog? x) (string? (Prog-name x)) (list? (Prog-expr* x))
        (andmap expr? (Prog-expr* x))))
 
+(pretty-print 
+  (vs-append 
+   (text "(let") 
+   (v-append
+    (h-append lparen 
+    (align 
+     (v-append 
+      (text "(kdfjakljfdsklgj kdjsfkajfkfjakdsj)") 
+      (h-append (text "(kadsfj dkjfk)") rparen))))
+    (h-append (indent 4 (text "bango!")) rparen)))))
