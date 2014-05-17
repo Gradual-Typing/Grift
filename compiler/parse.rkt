2145679daae27acecba02b4e3ae6ba549958299a
akuhlens <akuhlens@localhost.localdomain>
2014-05-16 20:00:23 -0400

    Finished the parsing of function types.
    Added the function desugar infix notation which remaps infix
    identifiers to prefix transformers. This does not seem to be
    tractable in the long run but it was a simple fix that seems
    flexible.
    
    At this point more unit tests need to be written both testing
    that correct syntax parses correctly and incorrect syntax is
    ruled out. Since I am running behind I think that I will finish
    type checking, cast insertion, and writing an interpreter for
    the ast before I revisit testing.

--- compiler/parse.rkt
+++ compiler/parse.rkt
@@ -19,15 +19,19 @@
     (for/list ([exp (in-list exp*)]) (parse-expr exp env)))
 
   (define (parse-expr exp env)
-    (let ((src (syntax-srcloc exp)) (exp^ (syntax-e exp)))
+    (let ((src (syntax-srcloc exp)) 
+	  (exp^ (syntax-e exp)))
     (cond
       [(pair? exp^) 
-       (let* ((rator (car exp^)) (rands (cdr exp^)) (e (syntax-e rator)))
+       (let* ((rator (car exp^)) 
+	      (rands (cdr exp^)) 
+	      (e (syntax-e rator)))
          (cond
-	  [(pair? e) (App src #f (parse-expr rator env) (parse-expr* rands env))]
+	  [(pair? e) (App src #f 
+			  (parse-expr rator env) 
+			  (parse-expr* rands env))]
 	  [(symbol? e)
-	   (match 
-	     (env-lookup env e (th (unbound src e (syntax->datum exp))))
+	   (match (env-lookup env e (env-err src e exp))
 	     [(Bnd:Var b)
 	      (let ((vsrc (syntax-srcloc rator)))
 		(App src #f (Var vsrc #f b) (parse-expr* rands env)))]
@@ -35,7 +39,7 @@
 	     [else (bad-syntax src e (strip exp))])]
            [else (bad-syntax src e (strip exp))]))]
       [(symbol? exp^)
-       (match (env-lookup env exp^ (th (unbound src exp^ 'expression)))
+       (match (env-lookup env exp^ (env-err src exp^ #'expression))
 	 [(Bnd:Var b) (Var src #f b)]
 	 [otherwise (bad-syntax src exp^ 'expression)])]
       [(constant? exp^) (Const src #f exp^)]
@@ -46,27 +50,48 @@
   (for/list ((ty (in-list ty*))) (parse-type ty env)))
 
 (define (parse-type ty env)
-  (let ((ty^ (syntax-e ty)))
+  (let ((ty^ (syntax-e ty))
+	(src (syntax-srcloc ty)))
     (cond
      [(pair? ty^)
-      (let* ((rator (car ty^))
-             (rands (cdr ty^))
-             (te (syntax-e rator)))
-        (cond
-         [(pair? te) (bad-syntax (syntax-srcloc ty) te ty)]
-         [(symbol? te)
-	  (match (env-lookup env te (th (unbound (syntax-srcloc ty)
-						 te (syntax->datum ty))))
-            [(Bnd:Type t) t]
-            [(Bnd:Type-Core t) (t ty env)]
-            [otherwise (bad-syntax (syntax-srcloc ty) te ty)])]
-         [else (bad-syntax (syntax-srcloc ty) te ty)]))] 
-    [(symbol? ty^)
-     (match 
-       (env-lookup env ty^ (th (unbound (syntax-srcloc ty) ty^ 'type)))
+      (handle-complex-type 
+       src ty (desugar-infix-notation ty^ env) env)]
+     [(symbol? ty^)
+     (match (env-lookup env ty^ (env-err src ty^ ty))
        [(Bnd:Type t) t]
        [otherwise (bad-syntax (syntax-srcloc ty) ty^ 'type)])]
+     [(constant? ty^) (bad-syntax (syntax-srcloc ty) ty 'type)]
     [else (cond-pass-error pass 'parse-type (strip ty))])))
+
+(define (desugar-infix-notation stx-ls env)
+  (match stx-ls
+    [(list (? (lambda (s) 
+		(match (env-lookup env (syntax-e s) #f)
+		  [(Bnd:Infix sym) #f]
+		  [otherwise #t])) args) ...
+	   (app (lambda (s) 
+		  (match (env-lookup env (syntax-e s) #f)
+		    [(Bnd:Infix sym) sym]
+		    [otherwise #f])) (and (not #f) sym)) 
+	   returns ...)
+     ;; This is ugly but I think that it is a less wasteful
+     ;; version of (syntax-e #`(#,sym #,args #,@returns))
+     ;; Just trying to get back to syntax-ls format after
+     ;; having completely destroyed it.
+     `(,#`#,sym ,#`#,args ,@returns)]
+    [otherwise stx-ls]))
+
+(define (handle-complex-type src ty ty-ls env)
+  (let* ((rator (car ty-ls))
+	 (te (syntax-e rator)))
+    (cond
+     [(pair? te) (bad-syntax src (strip rator) (strip ty))]
+     [(symbol? te)
+      (match (env-lookup env te (env-err src te ty))
+	;; could add type transformers here
+	[(Bnd:Type-Core t) (t ty env)]
+	[otherwise (bad-syntax src te (strip ty))])]
+     [else (bad-syntax src te ty)])))
 
 (define (lambda-transformer exp env)
   (define (from-to-fml fml env)
@@ -176,35 +201,38 @@
 
 (define (function-type-transformer stx env)
   (syntax-case stx ()
-    [(_ f t) (Function #'f #'t)]))
+    [(_ f . t) (Function (parse-type* (syntax-e #'f) env) 
+			 (parse-type* (syntax-e #'t) env))]))
 
 (struct Binding (value))
 (struct Bnd:Var  Binding ())
 (struct Bnd:Core Binding ())
 (struct Bnd:Type Binding ())
 (struct Bnd:Type-Core Binding ())
-
+(struct Bnd:Infix Binding ())
 
 
 
 (define core-env
-  (hasheq
-   'lambda    (Bnd:Core lambda-transformer)
-   'let  (Bnd:Core let-transformer)
-   'if   (Bnd:Core if-transformer)
-   ':    (Bnd:Core cast-transformer)
-   'Dyn  (Bnd:Type Dyn-Type)
-   'Int  (Bnd:Type Int-Type)
-   'Bool (Bnd:Type Bool-Type)
-   '->   (Bnd:Type-Core function-type-transformer)
-   '%<   (Bnd:Core lt-transformer)
-   '%>   (Bnd:Core gt-transformer)
-   '%=   (Bnd:Core eq-transformer)
-   '%<=  (Bnd:Core lteq-transformer)
-   '%>=  (Bnd:Core gteq-transformer)
-   '%*   (Bnd:Core mult-transformer)
-   '%+   (Bnd:Core plus-transformer)
-   '%-   (Bnd:Core minus-transformer)))
+  (let ((-> (gensym)))
+    (hasheq
+     'lambda (Bnd:Core lambda-transformer)
+     'let    (Bnd:Core let-transformer)
+     'if     (Bnd:Core if-transformer)
+     ':      (Bnd:Core cast-transformer)
+     'Dyn    (Bnd:Type Dyn-Type)
+     'Int    (Bnd:Type Int-Type)
+     'Bool   (Bnd:Type Bool-Type)
+     '->     (Bnd:Infix ->)
+     ->      (Bnd:Type-Core function-type-transformer)
+     '%<     (Bnd:Core lt-transformer)
+     '%>     (Bnd:Core gt-transformer)
+     '%=     (Bnd:Core eq-transformer)
+     '%<=    (Bnd:Core lteq-transformer)
+     '%>=    (Bnd:Core gteq-transformer)
+     '%*     (Bnd:Core mult-transformer)
+     '%+     (Bnd:Core plus-transformer)
+     '%-     (Bnd:Core minus-transformer))))
 
 (match stx-tree
   [(File name stx*)
@@ -216,30 +244,17 @@
 (define (syntax-srcloc exp)
   (srcloc (syntax-source exp) (syntax-line exp)
           (syntax-column exp) (syntax-position exp) (syntax-span exp)))
+
 (define-syntax strip
   (syntax-rules ()
     [(_ stx) (syntax->datum stx)]))
 
-(define (desugar-arrow-notation stx)
-    (match (syntax-e stx)
-      [(list (? (lambda (s) (not (eq? '-> (syntax-e s)))) 
-                (app desugar-arrow-notation args)) ...
-             (? (lambda (s) (eq? '-> (syntax-e s)))) 
-             (app desugar-arrow-notation return))
-       #`(-> #,args #,return)]
-      [(list (? (lambda (s) (not (eq? '-> (syntax-e s)))) 
-                     (app desugar-arrow-notation args)) ...
-                  (? (lambda (s) (eq? '-> (syntax-e s)))) 
-                  rest ...)
-       #`(-> #,args #,(desugar-arrow-notation rest))]
-      [otherwise stx]))
+(define-syntax env-err
+  (syntax-rules ()
+   ((_ src sym stx) (th (unbound src sym (strip stx))))))
     
-(module+ main
-  (local-require rackunit rackunit/text-ui)
-  (define tests
-    (test-suite "all"
-      (test-suite "desugar-arrow-notation"
-        (test-equal? "simple" (desugar-arrow-notation #'a) #'a)))))
+
+
 
 
 
