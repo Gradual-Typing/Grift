975b885b91a741c1bfe0b398d49110c21280f3f5
Andre Kuhlenschmidt <andre.kuhlenschmidt@gmail.com>
2016-06-28 12:29:13 -0400

    dicking around with generic forms

--- src/form-generics.rkt
+++ src/form-generics.rkt
@@ -2,10 +2,14 @@
 
 (require racket/generic)
 
-(provide form-map form->sexp language-form?)
+(provide form-map form-map-forward form-map-backward
+         form->sexp language-form?)
+
 (define-generics language-form
   #:fast-defaults
   ([null? (define (form-map f p) '())
+          (define (form-map-forward f p) '())
+          (define (form-map-backward f p) '())
           (define (form->sexp f) '())]
    [pair? (define/generic fmap  form-map)
           (define/generic fsexp form->sexp)
@@ -17,4 +21,12 @@
                   (fsexp (cdr f))))])
   ;; The generic interface form mapping over language forms
   [form-map language-form procs]
+  [form-map-forward language-form procs]
+  [form-map-backward language-form procs]
   [form->sexp language-form])
+
+(define (sexp->form x)
+  (match x
+    [(list (? symbol? ctr) xs ...)
+     ((hash-ref ctr (thunk (error 'sexp->form "unregistered ~a" ctr))) xs)]
+    [(list) '()]))
