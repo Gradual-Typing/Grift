3f966adc1bab8bf4184ce481890aa6d3fbaae17c
Deyaa <dalmahal@indiana.edu>
2017-02-01 01:41:46 -0500

    fix gambit ray

--- benchmark/suite/macro/src/gambit/ray.scm
+++ benchmark/suite/macro/src/gambit/ray.scm
@@ -1,7 +1,6 @@
-#!gsi-script -:d0
-
-;;; RAY -- Ray-trace a simple scene with spheres, generating a ".pgm" file.
+;;; RAY -- Ray-trace a simple scene with spheres.
 ;;; Translated to Scheme from Paul Graham's book ANSI Common Lisp, Example 9.8
+;;; And then translated to Schml by Deyaaeldeen Almahallawi
 
 (define (make-point x y z)
   (vector x y z))
@@ -12,160 +11,151 @@
 
 (define (sq x) (fl* x x))
 
-(define (mag x y z)
-  (flsqrt (fl+ (sq x) (sq y) (sq z))))
+(define (mag x y z) 
+  (flsqrt (fl+ (sq x) (fl+ (sq y) (sq z)))))
 
-(define (unit-vector x y z)
-  (let ((d (mag x y z)))
+(define (unit-vector x y z) 
+  (let ([d  (mag x y z)])
     (make-point (fl/ x d) (fl/ y d) (fl/ z d))))
 
-(define (distance p1 p2)
+(define (distance p1 p2) 
   (mag (fl- (point-x p1) (point-x p2))
        (fl- (point-y p1) (point-y p2))
        (fl- (point-z p1) (point-z p2))))
 
-(define (minroot a b c)
-  (if (flzero? a)
-      (fl/ (fl- c) b)
-      (let ((disc (fl- (sq b) (fl* 4.0 a c))))
-        (if (flnegative? disc)
-            #f
-            (let ((discrt (flsqrt disc))
-                  (minus-b (fl- b))
-                  (two-a (fl* 2.0 a)))
-              (flmin (fl/ (fl+ minus-b discrt) two-a)
-		     (fl/ (fl- minus-b discrt) two-a)))))))
-
-(define *world* '())
+(define *world* (make-vector 33 (vector 0.0 0.0 (vector 0.0 0.0 0.0))))
 
 (define eye (make-point 0.0 0.0 200.0))
 
-(define (tracer pathname res)
-  (call-with-output-file
-   pathname
-   (lambda (p)
-     (let ((extent (* res 100)))
-       (display "P2 " p)
-       (write extent p)
-       (display " " p)
-       (write extent p)
-       (display " 255" p)
-       (newline p)
-       (do ((y 0 (+ y 1)))
-           ((= y extent))
-         (do ((x 0 (+ x 1)))
-             ((= x extent))
-           (write (color-at
-                   (fl+ -50.0
-			(fl/ (exact->inexact x) (exact->inexact res)))
-                   (fl+ -50.0
-			(fl/ (exact->inexact y) (exact->inexact res))))
-                  p)
-           (newline p)))))))
+(define (tracer res)
+  (let ([extent (* res 100)])
+    (display "P2 ")
+    (write extent)
+    (display " ")
+    (write extent)
+    (display " 255")
+    (newline)
+    (do ((y 0 (+ y 1)))
+        ((= y extent))
+      (do ((x 0 (+ x 1)))
+          ((= x extent))
+        (write (color-at
+                (fl+ -50.0
+                     (fl/ (exact->inexact x) (exact->inexact res)))
+                (fl+ -50.0
+                     (fl/ (exact->inexact y) (exact->inexact res)))))
+        (newline)))))
 
 (define (color-at x y)
-  (let ((ray (unit-vector (fl- x (point-x eye))
+  (let ([ray (unit-vector (fl- x (point-x eye))
                           (fl- y (point-y eye))
-                          (fl- (point-z eye)))))
+                          (fl- (point-z eye)))])
     (inexact->exact (flround (fl* (sendray eye ray) 255.0)))))
 
 (define (sendray pt ray)
-  (let* ((x (first-hit pt ray))
-         (s (vector-ref x 0))
-         (int (vector-ref x 1)))
-    (if s
-        (fl* (lambert s int ray)
-	     (surface-color s))
-        0.0)))
+  (let ([x (loop pt ray 0
+                 (vector-length *world*)
+                 *world*
+                 #f
+                 #f
+                 1e308)])
+    (let ([s (vector-ref x 0)]
+          [int (vector-ref x 1)])
+      (if s
+          (fl* (lambert s int ray)
+               (sphere-color s))
+          0.0))))
 
-(define (first-hit pt ray)
-  (let loop ((lst *world*) (surface #f) (hit #f) (dist 1e308))
-    (if (null? lst)
-        (vector surface hit)
-        (let ((s (car lst)))
-          (let ((h (intersect s pt ray)))
-            (if h
-                (let ((d (distance h pt)))
-                  (if (fl< d dist)
-                      (loop (cdr lst) s h d)
-                      (loop (cdr lst) surface hit dist)))
-                (loop (cdr lst) surface hit dist)))))))
+(define (loop pt ray index lst-len lst surface hit dist)
+  (if (= index lst-len)
+      (vector surface hit)
+      (let ([s (vector-ref lst index)])
+        (let ([xr  (point-x ray)]
+              [yr  (point-y ray)]
+              [zr  (point-z ray)]
+              [sc  (sphere-center s)])
+          (let ([a  (fl+ (sq xr) (fl+ (sq yr) (sq zr)))]
+                [b  (fl* 2.0
+                         (fl+ (fl* (fl- (point-x pt) (point-x sc)) xr)
+                              (fl+ (fl* (fl- (point-y pt) (point-y sc)) yr)
+                                   (fl* (fl- (point-z pt) (point-z sc)) zr))))]
+                [c  (fl+ (fl+ (sq (fl- (point-x pt) (point-x sc)))
+                              (sq (fl- (point-y pt) (point-y sc))))
+                         (fl+ (sq (fl- (point-z pt) (point-z sc)))
+                              (fl- (sq (sphere-radius s)))))])
+            (if (flzero? a)
+                (let ([n  (fl/ (fl- c) b)])
+                  (let ([h (make-point (fl+ (point-x pt) (fl* n xr))
+                                       (fl+ (point-y pt) (fl* n yr))
+                                       (fl+ (point-z pt) (fl* n zr)))])
+                    (let ([d (distance h pt)])
+                      (if (fl< d dist)
+                          (loop pt ray (+ index 1) lst-len lst s h d)
+                          (loop pt ray (+ index 1) lst-len lst surface hit dist)))))
+                (let ([disc  (fl- (sq b) (fl* 4.0 (fl* a c)))])
+                  (if (flnegative? disc)
+                      (loop pt ray (+ index 1) lst-len lst surface hit dist)
+                      (let ([discrt  (flsqrt disc)]
+                            (minus-b  (fl- b))
+                            (two-a  (fl* 2.0 a)))
+                        (let ([n (flmin (fl/ (fl+ minus-b discrt) two-a)
+                                        (fl/ (fl- minus-b discrt) two-a))])
+                          (let ([h (make-point (fl+ (point-x pt) (fl* n xr))
+                                               (fl+ (point-y pt) (fl* n yr))
+                                               (fl+ (point-z pt) (fl* n zr)))])
+                            (let ([d  (distance h pt)])
+                              (if (fl< d dist)
+                                  (loop pt ray (+ index 1) lst-len lst s h d)
+                                  (loop pt ray (+ index 1) lst-len lst surface hit dist))))))))))))))
+
 
 (define (lambert s int ray)
-  (let ((n (normal s int)))
+  (let ([n (sphere-normal s int)])
     (flmax 0.0
-	   (fl+ (fl* (point-x ray) (point-x n))
-		(fl* (point-y ray) (point-y n))
-		(fl* (point-z ray) (point-z n))))))
+           (fl+ (fl* (point-x ray) (point-x n))
+                (fl+ (fl* (point-y ray) (point-y n))
+                     (fl* (point-z ray) (point-z n)))))))
 
-(define (make-sphere color radius center)
+(define (make-sphere color radius center) 
   (vector color radius center))
 
-(define (sphere-color s) (vector-ref s 0))
-(define (sphere-radius s) (vector-ref s 1))
-(define (sphere-center s) (vector-ref s 2))
+(define (sphere-color s) 
+  (vector-ref s 0))
+(define (sphere-radius s) 
+  (vector-ref s 1))
+(define (sphere-center s) 
+  (vector-ref s 2))
 
-(define (defsphere x y z r c)
-  (let ((s (make-sphere c r (make-point x y z))))
-    (set! *world* (cons s *world*))
-    s))
+(define (defsphere i x y z r c) 
+  (let ([s (make-sphere c r (make-point x y z))])
+    (begin
+      (vector-set! *world* i s)
+      s)))
 
-(define (surface-color s)
-  (sphere-color s))
-
-(define (intersect s pt ray)
-  (sphere-intersect s pt ray))
-
-(define (sphere-intersect s pt ray)
-  (let* ((xr (point-x ray))
-         (yr (point-y ray))
-         (zr (point-z ray))
-         (c (sphere-center s))
-         (n (minroot
-             (fl+ (sq xr) (sq yr) (sq zr))
-             (fl* 2.0
-		  (fl+ (fl* (fl- (point-x pt) (point-x c)) xr)
-		       (fl* (fl- (point-y pt) (point-y c)) yr)
-		       (fl* (fl- (point-z pt) (point-z c)) zr)))
-             (fl+ (sq (fl- (point-x pt) (point-x c)))
-		  (sq (fl- (point-y pt) (point-y c)))
-		  (sq (fl- (point-z pt) (point-z c)))
-		  (fl- (sq (sphere-radius s)))))))
-    (if n
-        (make-point (fl+ (point-x pt) (fl* n xr))
-                    (fl+ (point-y pt) (fl* n yr))
-                    (fl+ (point-z pt) (fl* n zr)))
-        #f)))
-
-(define (normal s pt)
-  (sphere-normal s pt))
-
-(define (sphere-normal s pt)
-  (let ((c (sphere-center s)))
+(define (sphere-normal s pt) 
+  (let ([c (sphere-center s)])
     (unit-vector (fl- (point-x c) (point-x pt))
                  (fl- (point-y c) (point-y pt))
                  (fl- (point-z c) (point-z pt)))))
 
-(define (ray-test . opt)
-  (set! *world* '())
-  (defsphere 0.0 -300.0 -1200.0 200.0 0.8)
-  (defsphere -80.0 -150.0 -1200.0 200.0 0.7)
-  (defsphere 70.0 -100.0 -1200.0 200.0 0.9)
-  (do ((x -2 (+ x 1)))
-      ((> x 2))
-    (do ((z 2 (+ z 1)))
-        ((> z 7))
-      (defsphere
-        (fl* (exact->inexact x) 200.0)
-        300.0
-        (fl* (exact->inexact z) -400.0)
-        40.0
-        0.75)))
-  (tracer "spheres.pgm" (if (null? opt) 1 (car opt))))
-
-(define (run)
-  (ray-test 1)
-  'ok)
-
-(define (main . args)
-  (run))
+(define (main)
+  (begin
+    (let ([counter (box 29)])
+      (begin
+        (defsphere 32 0.0 -300.0 -1200.0 200.0 0.8)
+        (defsphere 31 -80.0 -150.0 -1200.0 200.0 0.7)
+        (defsphere 30 70.0 -100.0 -1200.0 200.0 0.9)
+        (do ((x -2 (+ x 1)))
+            ((> x 2))
+          (do ((z 2 (+ z 1)))
+              ((> z 7))
+            (defsphere
+              (unbox counter)
+              (fl* (exact->inexact x) 200.0)
+              300.0
+              (fl* (exact->inexact z) -400.0)
+              40.0
+              0.75)
+            (set-box! counter (- (unbox counter) 1))))))
+    (tracer 1)))
+(main)
