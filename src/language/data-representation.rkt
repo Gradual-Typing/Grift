633cf4910ddf431251e7f05b37941ebf45bf4036
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2017-10-26 23:19:45 -0400

    generating C header file with constants used in Grift

--- src/language/data-representation.rkt
+++ src/language/data-representation.rkt
@@ -1,5 +1,5 @@
 #lang typed/racket
-(require (for-syntax racket/syntax))
+(require (for-syntax racket/syntax "c-helpers.rkt"))
 (require "../language/forms.rkt")
 (provide (all-defined-out))
 
@@ -8,6 +8,12 @@
     [(_ (name val) ...)
      (let ([fmt (lambda (x) (format-id stx "data:~a" x))])
        (with-syntax ([(fmt-name ...) (map fmt (syntax->list #'(name ...)))])
+         (append-to-constants.h
+          'replace
+          (lambda (in)
+            (map (define-line in)
+                 (syntax-e #'(name ...))
+                 (syntax-e #'(val ...)))))
          #'(begin
              (define fmt-name val) ...
              (define name (Quote fmt-name)) ...)))]))
