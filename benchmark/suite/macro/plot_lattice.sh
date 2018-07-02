54b337968099b15b4c7205ac5930e9200a0a0aae
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2018-07-01 21:35:23 -0400

    adding cumulative plots

--- benchmark/suite/macro/plot_lattice.sh
+++ benchmark/suite/macro/plot_lattice.sh
@@ -58,19 +58,25 @@ function plot_benchmark()
     local plot_dir="${OUT_DIR}/${ct}/${name}"
     local alls_dir="${OUT_DIR}/${ct}/alls"
     local rt_casts_dir="${OUT_DIR}/${ct}/rt_casts"
+    local cum_perf_dir="${OUT_DIR}/${ct}/cum_perf"
 
     mkdir -p "$plot_dir"
     mkdir -p "$alls_dir"
     mkdir -p "$rt_casts_dir"
+    mkdir -p "$cum_perf_dir"
 
     local lpc_fig="${plot_dir}/lpc.png"
     local rt_fig="${plot_dir}/rt.png"
     local mono_fig="${plot_dir}/mono.png"
     local casts_fig="${plot_dir}/casts.png"
     local all_fig="${plot_dir}/all.png"
+    local cum_perf_lattice_fig="${cum_perf_dir}/${name}.png"
 
     local logfile1="${DATA_DIR}/${name}${c1}.log.sorted"
     local logfile3="${DATA_DIR}/${name}${c2}.log.sorted"
+
+    local logfile2="${DATA_DIR}/${name}${c1}.csv1"
+    local logfile4="${DATA_DIR}/${name}${c2}.csv1"
 
     if [ ! -f "${DATA_DIR}/${name}${c1}.log" ]; then
 	return
@@ -99,13 +105,26 @@ function plot_benchmark()
     dyn_mean=$(cat "${TMP_DIR}/dyn/${name}${disk_aux_name}17.runtime")
     static_mean=$(cat "${TMP_DIR}/static/${name}${disk_aux_name}.static.runtime")
 
+    tail -n +2 "$logfile1" |\
+	awk -F "," -v x="$dyn_mean" '{printf "%4.2f\n", $3/$x }' | \
+        sort | \
+        uniq -c | \
+        awk ' { t = $1; $1 = $2; $2 = t; print; } ' | \
+        awk '{ $1=$1" ,";; print }' > "$logfile2"
+
+    tail -n +2 "$logfile3" |\
+	awk -F "," -v x="$dyn_mean" '{printf "%4.2f\n", $3/$x }' | \
+        sort | \
+        uniq -c | \
+        awk ' { t = $1; $1 = $2; $2 = t; print; } ' | \
+        awk '{ $1=$1" ,";; print }' > "$logfile4"
+
     gnuplot -e "set datafile separator \",\";"`
             `"set terminal pngcairo size 1280,960"`
             `"   noenhanced color font 'Verdana,26' ;"`
             `"set output '${lpc_fig}';"`
-            `"set key left font 'Verdana,20';"`
+            `"set key opaque bottom left box vertical width 1 height 1 maxcols 1 spacing 1 font 'Verdana,20';"`
             `"set title \"${printname}\";"`
-	    `"set logscale y;"`
             `"set xlabel \"How much of the code is typed\";"`
             `"set ylabel \"Longest proxy chain\";"`
             `"plot '${logfile1}' using 2:8 with points"` 
@@ -117,7 +136,7 @@ function plot_benchmark()
             `"set terminal pngcairo size 1280,960"`
             `"   enhanced color font 'Verdana,26' ;"`
             `"set output '${rt_fig}';"`
-            `"set key top right box vertical width 1 height 1 maxcols 1 spacing 1 font 'Verdana,20';"`
+            `"set key opaque bottom left box vertical width 1 height 1 maxcols 1 spacing 1 font 'Verdana,20';"`
             `"set title \"${printname}\";"`
    	    `"stats '${logfile1}' nooutput;"`
 	    `"set xrange [0:STATS_records+10];"`
@@ -139,9 +158,8 @@ function plot_benchmark()
             `"set terminal pngcairo size 1280,960"`
             `"   enhanced color font 'Verdana,26' ;"`
             `"set output '${casts_fig}';"`
-            `"set key left font 'Verdana,20';"`
+            `"set key opaque bottom left box vertical width 1 height 1 maxcols 1 spacing 1 font 'Verdana,20';"`
             `"set title \"${printname}\";"`
-	    `"set logscale y;"`
             `"set xlabel \"How much of the code is typed\";"`
             `"set ylabel \"Runtime casts count\";"`
             `"plot '${logfile1}' using 2:7 with points"` 
@@ -244,6 +262,19 @@ function plot_benchmark()
             `"${static_mean} lw 2 dt 2 lc \"blue\" title 'Static Grift',"`
             `"${dyn_mean} lw 2 lt 1 lc \"red\" title 'Dynamic Grift';"
 
+        gnuplot -e "set datafile separator \",\"; set terminal pngcairo "`
+                `"enhanced color font 'Verdana,10' ;"`
+                `"set output '${cum_perf_lattice_fig}';"`
+                `"set border 15 back;"`
+                `"set title \"${printname}\";"`
+                `"stats '${logfile1}' nooutput;"`
+                `"set yrange [0:STATS_records];"`
+                `"set xrange [0:10];"`
+                `"set xtics nomirror (\"1x\" 1,\"2x\" 2,\"3x\" 3,\"4x\" 4,\"5x\" 5, \"6x\" 6,\"7x\" 7, \"8x\" 8, \"9x\" 9, \"10x\" 10, \"15x\" 15, \"20x\" 20);"`
+                `"set ytics nomirror 0,200;"`
+                `"set arrow from 1,graph(0,0) to 1,graph(1,1) nohead lc rgb \"black\" lw 2;"`
+                `"plot '${logfile2}' using 1:2 with lines lw 2 dt 4 title '${c1t}' smooth cumulative,"`
+                `"     '${logfile4}' using 1:2 with lines lw 2 dt 2 title '${c2t}' smooth cumulative"
     cp "${all_fig}" "${alls_dir}/${name}.png"
     cp "${mono_fig}" "${rt_casts_dir}/${name}.png"
 }
