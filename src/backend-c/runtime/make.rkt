3f28e97a617d27bb3fd85377b73dcd25a696dd81
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2018-09-24 20:31:00 -0400

    fix bug in src/backend-c/runtime/make.rkt

--- src/backend-c/runtime/make.rkt
+++ src/backend-c/runtime/make.rkt
@@ -2,8 +2,8 @@
 (require racket/system)
 (provide post-installer)
 
-(define (post-installer collections-top-path this-collection-path)
+(define (post-installer _collections-top-path this-collection-path)
   (define pwd (build-path this-collection-path "src/backend-c/runtime"))
-  (current-directory pwd)
-  (unless (system "make" #:set-pwd? #t)
-    (error (format "Running make failed in ~a" pwd))))
+  (parameterize ([current-directory pwd])
+    (unless (system "make" #:set-pwd? #t)
+      (printf (format "\nError: Running make failed in ~a" pwd)))))
