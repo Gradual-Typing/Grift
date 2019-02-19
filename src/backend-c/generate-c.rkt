b37d55ea1399c11c6c1bef83c5c009e51791197c
Andre Kuhlenschmidt <andre.kuhlenschmidt@gmail.com>
2019-02-19 11:10:36 -0500

    deleting more language files

--- src/backend-c/generate-c.rkt
+++ src/backend-c/generate-c.rkt
@@ -11,11 +11,11 @@
 ;; The define-pass syntax
 (require
  "../errors.rkt"
+ "../casts/cast-profiler.rkt"
  "../configuration.rkt"
  "../language/forms.rkt"
  "../macros.rkt"
- "runtime-location.rkt"
- "../casts/cast-profiler.rkt")
+ "runtime-location.rkt")
 
 ;; Only the pass is provided by this module
 (provide generate-c)
