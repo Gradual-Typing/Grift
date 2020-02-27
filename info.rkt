f05696fd366f4bd62058d07d73969307bc134820
Andre Kuhlenschmidt <andre.kuhlenschmidt@gmail.com>
2020-02-27 11:39:31 -0500

    LLVM Backend (#92)
    * config adding sham
    
    * fix travis config for racket dependencies
    
    * add llvm to travis
    
    * fixing according to comments

--- info.rkt
+++ info.rkt
@@ -1,10 +1,16 @@
 #lang info
 (define collection "grift")
 (define version "0.1")
+(define deps
+  '(;; Since sham isn't on the package server yet we use a direct link to github
+    "https://github.com/rjnw/sham.git/?path=sham-llvm#3dd8cacb82bb4c77b447f76d984fee4f2aae9f0a"
+    "https://github.com/rjnw/sham.git/?path=sham-lib#3dd8cacb82bb4c77b447f76d984fee4f2aae9f0a"
+    "https://github.com/rjnw/scf.git/?path=scf-lib#a9405041f636ac7c4368b736d10f3f8a858eed74"))
 (define racket-launcher-names '("grift" "grift-bench" "grift-configs"))
 (define racket-launcher-libraries '("main.rkt" "benchmark/bench.rkt" "benchmark/configs.rkt"))
 (define post-install-collection "src/backend-c/runtime/make.rkt")
-(define compile-omit-files '("src/backend-c/runtime/make.rkt"))
+(define compile-omit-files
+  '("src/backend-c/runtime"))
 (define clean
   '("src/backend-c/runtime/boehm-gc-install"
     "src/backend-c/runtime/cast-profiler.o"
---
+++ tests/one-off/sham-install.rkt
@@ -0,0 +1,64 @@
+#lang racket
+;; The intention of the test is to ensure that sham is installed and
+;; that the llvm portion is working. Does our install script
+;; successfully install sham and if the llvm is present can we
+;; generate code. Sham is build around a JIT system which I don't care
+;; about working correctly, and in fact doesn't work on my laptop. The
+;; sham test suite would check the functionality of the JIT.
+(module+ test
+  (require
+   racket/runtime-path
+   rackunit
+   ffi/unsafe
+   sham/ast
+   sham/ir
+   sham/env
+   sham/llvm/ffi/all
+   sham/llvm/llvm-config)
+
+  (llvm-initialize-all)
+  (define context (LLVMContextCreate))
+
+  (define module
+    (env-get-llvm-module
+     (build-llvm-module
+      (dmodule
+       (hash)
+       'module
+       (list
+        (function^
+         'main () i64
+         (return (ui64 42))))))))
+
+  (define-runtime-path basename "./test42")
+  (define bc-path (path-add-extension basename ".bc"))
+  (define bin-path (llvm-config "--bindir"))
+  (define asm-path (path-add-extension basename ".s"))
+
+  (check-equal?
+   0
+   (LLVMWriteBitcodeToFile module (path->string bc-path)))
+
+  (check-true
+   (system*
+    (build-path bin-path "llc")
+    "-O3"
+    "--relocation-model=pic"
+    bc-path))
+
+  (check-true
+   (system*
+    (build-path bin-path "clang")
+    asm-path
+    "-o"
+    basename))
+
+  (check-equal?
+   42
+   (system*/exit-code basename))
+
+  (delete-file basename)
+  (delete-file bc-path)
+  (delete-file asm-path)
+  (LLVMDisposeModule module)
+  (LLVMContextDispose context))
