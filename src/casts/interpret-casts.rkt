54c94e7fb5356e42d49e0bad8b8f7b6b81f4f12f
Yuya Tsuda <tsuda@fos.kuis.kyoto-u.ac.jp>
2020-03-16 14:28:02 +0900

    Use parameterize to turn off optimize-first-order-coercions?

--- src/casts/interpret-casts.rkt
+++ src/casts/interpret-casts.rkt
@@ -34,15 +34,12 @@
         [(Coercions)
          (cond
           [(enable-crcps?)
-           ;; avoid removing id coercions around injection/projection:
-           (optimize-first-order-coercions? #f)  ; should be parameterize?
            (define-values (icc apply-coercion-uid compose-coercions-uid)
              (interpret-casts/coercions))
            (compose1 (coercion-passing-trans #:apply-coercion-uid apply-coercion-uid
                                              #:compose-coercions-uid compose-coercions-uid)
                      icc)]
           [else
-           (optimize-first-order-coercions? #t)  ; should be parameterize?
            (define-values (icc apply-coercion-uid compose-coercions-uid)
              (interpret-casts/coercions))
            icc])]
@@ -51,8 +48,13 @@
         [else (error 'grift/interpret-casts
                      "unexpected cast representation: ~a"
                      (cast-representation))]))
-    
-    (define new-e (ic-expr! e))
+
+    (define new-e
+      (cond [(enable-crcps?)
+             ;; avoid removing id coercions around injection/projection:
+             (parameterize ([optimize-first-order-coercions? #f])
+               (ic-expr! e))]
+             [else (ic-expr! e)]))
     
     (define rt-bindings  (cast-runtime-code-bindings))
     (define rt-constants : (Option CoC3-Bnd*)
